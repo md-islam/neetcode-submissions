eae8ea316c98a3a306605ae18bc027d200ab4224
MD <aeinul.islam@gmail.com>
2026-06-16 00:19:14 -0700

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.java
@@ -0,0 +1,51 @@
+class PrefixTree {
+
+    class TrieNode{
+        boolean isWord;
+        Map<Character, TrieNode> children;
+
+        TrieNode() {
+            this.isWord = false;
+            this.children = new HashMap<Character, TrieNode>();
+        }
+    }
+
+    TrieNode root;
+
+    public PrefixTree() {
+         root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode current = root;
+        for(char c: word.toCharArray()){
+            if(!current.children.containsKey(c)){
+                current.children.put(c, new TrieNode());
+            }
+            current = current.children.get(c);
+        }
+        current.isWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode current = root;
+        for(char c: word.toCharArray()){
+            if(!current.children.containsKey(c)){
+                return false;
+            }
+            current = current.children.get(c);
+        }
+        return current.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode current = root;
+        for(char c: prefix.toCharArray()){
+            if(!current.children.containsKey(c)){
+                return false;
+            }
+           current = current.children.get(c);
+        }
+        return true;
+    }
+}
